63b6938ee5281dce38f0c0d634ccc815f7dc25b2
Ekaterina <ekaterinalarionova25@gmail.com>
2023-08-25 18:58:53 +0300

    add pagination

--- src/components/ProjectsTable.vue
+++ src/components/ProjectsTable.vue
@@ -3,12 +3,8 @@
         <h1>Таблица проектов</h1>
         <p>Общее количество: {{ projects.count }}</p>
         <div class="flex flex-col">
-            <SearchComponent
-                    :query="query"
-            />
-            <SelectorComponent
-                    :query="query"
-            />
+            <SearchComponent :query="query" />
+            <SelectorComponent :query="query" />
         </div>
         <div class="flex flex-col">
             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -25,16 +21,16 @@
                             </thead>
                             <tbody v-if="loading.status">
                             <tr>
-                                <LoadingSvg/>
+                                <LoadingSvg />
                             </tr>
                             </tbody>
                             <tbody v-else>
                             <tr
-                                    class="border-b dark:border-neutral-500"
-                                    v-for="(project, key) in filter"
-                                    :key="key"
+                                class="border-b dark:border-neutral-500"
+                                v-for="(project, index) in paginatedProjects"
+                                :key="project.id"
                             >
-                                <td class="whitespace-nowrap px-6 py-4 font-medium">{{ key + 1 }}</td>
+                                <td class="whitespace-nowrap px-6 py-4 font-medium">{{ (currentPage - 1) * pageSize + index + 1 }}</td>
                                 <td class="whitespace-nowrap px-6 py-4">{{ project.id }}</td>
                                 <td class="whitespace-nowrap px-6 py-4">{{ project.title }}</td>
                                 <td class="whitespace-nowrap px-6 py-4">{{ project.status }}</td>
@@ -45,12 +41,23 @@
                 </div>
             </div>
         </div>
+
+        <div class="flex justify-center mt-4">
+            <button
+                v-for="page in totalPages"
+                :key="page"
+                @click="setCurrentPage(page)"
+                :class="{ 'bg-blue-500 text-white px-4 py-2 mx-1 rounded hover:bg-blue-600': page !== currentPage, 'bg-blue-300 text-blue-800 px-4 py-2 mx-1 rounded': page === currentPage }"
+            >
+                {{ page }}
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup>
 import axios from "axios";
-import {ref, onMounted, computed, reactive} from 'vue'
+import { ref, onMounted, computed, reactive } from "vue";
 import SearchComponent from "@/components/SearchComponent.vue";
 import SelectorComponent from "@/components/SelectorComponent.vue";
 import LoadingSvg from "@/components/LoadingSvg.vue";
@@ -62,23 +69,25 @@ let loading = reactive({
     status: false,
 });
 let query = reactive({
-    search: '',
+    search: "",
     selector: null,
 });
+let currentPage = ref(1);
+const pageSize = 7;
 
 onMounted(async () => {
     await fetchData();
-})
+});
 
 async function fetchData() {
     loading.status = true;
-    const res = await axios.get('https://dev.aicap.tech/api/v1/interview/projects/');
+    const res = await axios.get("https://dev.aicap.tech/api/v1/interview/projects/");
 
-    //here setTimeout done for showing loading status. without it loading is instant
+    // here setTimeout done for showing loading status. without it loading is instant
     setTimeout(() => {
         projects.value = res.data;
         loading.status = false;
-    }, 2000)
+    }, 2000);
 }
 
 const filter = computed(() => {
@@ -94,13 +103,35 @@ const getSearchableProjects = () => {
     return projects.value.results.filter((el) => {
         return el.title.toLowerCase().indexOf(query.search.toLowerCase()) !== -1;
     });
-}
+};
 
 const getSelectableProjects = () => {
     return projects.value.results.filter((el) => {
         return el.status.indexOf(query.selector) !== -1;
     });
-}
+};
+
+const setCurrentPage = (page) => {
+    if (page >= 1 && page <= totalPages.value) {
+        currentPage.value = page;
+    }
+};
+
+const paginatedProjects = computed(() => {
+    const startIndex = (currentPage.value - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    if(filter.value){
+        return filter.value.slice(startIndex, endIndex);
+    } else {
+        return null;
+    }
+});
+
+const totalPages = computed(() => {
+    if(filter.value) {
+        return Math.ceil(filter.value.length / pageSize);
+    }
+});
 </script>
 
 <style scoped>
